62715e5e67082f1cdbeceba7e7b1ac2535513840
Laptop <babalielchin@gmail.com>
2022-11-07 01:13:02 +0400

    Deleted useless references in Form1 class.

--- IELTSpeaking/Form1.cs
+++ IELTSpeaking/Form1.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using IELTSpeaking.Helpers.Speech;
-using NAudio;
-using NAudio.Wave;
-using NAudio.CoreAudioApi;
-using IELTSpeaking.Helpers;
 
 namespace IELTSpeaking
 {
