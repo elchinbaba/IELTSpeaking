37443d2879dcfed89991967ea9a974afd77a7128
Laptop <babalielchin@gmail.com>
2022-11-07 18:28:24 +0400

    Divided the project into stages and implemented functionalities in these parts.

---
+++ IELTSpeaking/Stages/Stage.cs
@@ -0,0 +1,29 @@
+﻿using IELTSpeaking.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IELTSpeaking.Stages
+{
+    class Stage
+    {
+        public static async Task Go(string question)
+        {
+            try
+            {
+                await Game.Speak(question);
+
+                Controller.HideControl(Game.iELTSpeaking.startBtn);
+
+                await Game.Play(time: 10);
+            }
+            catch
+            {
+                MessageBox.Show("No question");
+            }
+        }
+    }
+}
---
+++ IELTSpeaking/Stages/Stage1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IELTSpeaking.Helpers;
+using IELTSpeaking.Helpers.Speech;
+
+namespace IELTSpeaking.Stages
+{
+    class Stage1 : IStage
+    {
+        List<string> questions;
+        int currentQuestionID;
+        public Stage1(List<string> questions)
+        {
+            this.questions = questions;
+            currentQuestionID = 0;
+        }
+        public async Task Go()
+        {
+            await Stage.Go(questions[currentQuestionID]);
+        }
+        public IStage NextStage()
+        {
+            if (questions.Count > 0 && currentQuestionID != questions.Count - 1)
+            {
+                currentQuestionID++;
+                return this;
+            }
+            return new Stage2(Game.questionsPart2);
+        }
+    }
+}
